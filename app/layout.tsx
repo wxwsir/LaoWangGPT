3547c3fe74980dcb61a195539d259f99ec922b6e
wxwsir <83161814+wxwsir@users.noreply.github.com>
2023-11-02 18:44:48 +0800

    layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,18 @@ export default function RootLayout({
         <script src="/serviceWorkerRegister.js" defer></script>
       </head>
       <body>
-          <a href="https://www.kda-xinxinzi.com/" target="_blank">刘路大校</a>
+           <footer>
+      <div align="center">
+        Copyright © 2021
+        <script>
+          document.write(" - " + new Date().getFullYear());
+        </script>
+        <a href="https://www.kda-xinxinzi.com/" target="_blank">刘路大校</a>
+      </div>
+      <div align="center">
+        <a href="https://beian.miit.gov.cn/" target="_blank">吉ICP备2022001026号-1</a>
+      </div>
+    </footer>
       </body>
     </html>
   );
